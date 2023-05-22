0bb00026bb618b91b76ca864a2678f0882bad852
Manu <67543265+MarcioEmanuel@users.noreply.github.com>
2023-05-22 20:44:11 -0300

    Copiat texto do discord para a area de transferencia

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ function swap() {
     if (btn.textContent === "Discord") {
         btn.textContent = "M.Emanuel#6725";
         btn.value = "M.Emanuel#6725";
+        var texto = "M.Emanuel#6725";
+        /* Cria um elemento de área de transferência temporário */
+        var textareaTemp = document.createElement("textarea");
+        textareaTemp.value = texto;
+        /* Adiciona o elemento temporário ao corpo do documento */
+        document.body.appendChild(textareaTemp);
+        /* Seleciona o texto no elemento de área de transferência temporário */
+        textareaTemp.select();
+        textareaTemp.setSelectionRange(0, textareaTemp.value.length);
+        /* Copia o texto selecionado para a área de transferência */
+        document.execCommand("copy");
+        /* Remove o elemento de área de transferência temporário */
+        document.body.removeChild(textareaTemp);
+        /* Exibe uma mensagem de sucesso */
+        alert(texto + " Copiado");
+
     } else {
         btn.textContent = "Discord";
         btn.value = "Discord";
